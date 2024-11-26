015e37f18d8b6400b35a8caeb457af8950d76ffe
Dereckr <dereckromos@gmail.com>
2024-11-26 15:01:22 -0400

    update

---
+++ routes/accountRoute.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = new express.Router();
+const utilities = require("../utilities");
+const accountController = "../controllers/accountController";
+
+//route to build Login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+
+module.exports = router;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,14 +1,18 @@
 // Needed Resources
 const express = require("express");
 const router = new express.Router();
+const utilities = require("../utilities");
 
 // const routerCar = new express.Router();
 const invController = require("../controllers/invController");
 //route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId)
+);
 //route to build car specifications
 router.get("/detail/:carId", invController.buildByCar);
 //route for error
-router.get("/errorTrigger", invController.errorTrigger);
+router.get("/errorTrigger", utilities.handleErrors(invController.errorTrigger));
 
 module.exports = router;
