720a252be11f7cf5afbba2a338b805b7bbd8e587
Dereckr <dereckromos@gmail.com>
2024-11-26 15:03:56 -0400

    update

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const utilities = require("../utilities");
-const accountController = "../controllers/accountController";
+const accountController = require("../controllers/accountController");
 
 //route to build Login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
